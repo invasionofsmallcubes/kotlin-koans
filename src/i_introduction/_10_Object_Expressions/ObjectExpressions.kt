b1e75e4f38eb3ec1cff029c2d3aac76455c1acf0
Emanuele Ianni <emanuele.ianni@rumbo.com>
2016-02-27 21:01:51 +0100

    to 10

--- src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -18,6 +18,10 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, //{ t, k -> k - t  })
+    object : Comparator<Int> {
+        override fun compare(x: Int, y: Int) = y - x
+    })
+
     return arrayList
 }
