24431dff0adc96f334cf2c192217e4e22eaef6ea
Emanuele Ianni <emanuele.ianni@rumbo.com>
2016-03-04 15:16:31 +0100

    to 35

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -8,16 +8,47 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
     }
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other);
+}
+
+operator fun MyDate.plus(timeInterval: TimeInterval) : MyDate {
+    return this.addTimeIntervals(timeInterval, 1)
+}
+
+operator fun MyDate.plus(timeInterval: RepeatedTimeInterval) : MyDate {
+    return this.addTimeIntervals(timeInterval.timeInterval , timeInterval.number)
+}
 
 enum class TimeInterval {
     DAY,
     WEEK,
-    YEAR
+    YEAR;
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+
+    override fun iterator(): Iterator<MyDate> {
+        return DateIterator(this)
+    }
+
     operator fun contains(d: MyDate): Boolean {
         return d > start && d < endInclusive
+    }
+}
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+
+    var current = dateRange.start
+
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
     }
 }
